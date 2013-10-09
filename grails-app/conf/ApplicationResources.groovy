483d141153684528e6ec5e92f88c306c82b9ff7c
Tiff Ting <tiff@bazaarvoice.com>
2013-10-09 14:42:31 -0700

    Add example D3 nations script

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,5 +2,8 @@ modules = {
     application {
         resource url:'js/application.js'
         resource url:'js/google-jsapi.js'
+        resource url:'js/d3.v3.js'
+
+        resource url:'css/application.css'
     }
 }
