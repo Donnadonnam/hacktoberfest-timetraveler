7122b5ddca8d5e18e2261d27b0519467326b03e1
Donnadonnam <123767522+Donnadonnam@users.noreply.github.com>
2023-02-09 21:09:34 -0600

    Donnadonnam/github.dev/Donnadonnam

--- test/unit/com/bazaarvoice/hacktoberfest/timetraveler/ProductTests.groovy
+++ test/unit/com/bazaarvoice/hacktoberfest/timetraveler/ProductTests.groovy
@@ -1,15 +1,1 @@
-package com.bazaarvoice.hacktoberfest.timetraveler
-
-import com.bazaarvoice.hacktoberfest.timetraveler.domain.Product
-import grails.test.mixin.*
-
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(Product)
-class ProductTests {
-
-    void testSomething() {
-       fail "Implement me"
-    }
-}
+#  
